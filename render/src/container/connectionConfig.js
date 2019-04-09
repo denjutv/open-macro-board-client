09f6280c44d1c527cbed22bf0b0ee8b9920939a8
denjutv <info@denju.tv>
2019-04-09 22:10:17 +0200

    check connection config bevore dispatching save event

--- render/src/container/connectionConfig.js
+++ render/src/container/connectionConfig.js
@@ -10,7 +10,49 @@ const mapStateToProps = ( state, ownProps ) =>
 const mapDispatchToProps = ( dispatch, ownProps ) =>
 ({
     updateConnection: event => dispatch( updateConfigConnection( event.target.name, event.target.value ) ),
-    saveCurrentConnection: currentConnection => dispatch( saveCurrentConnection( Object.assign( {}, currentConnection ) ) )
+    saveCurrentConnection: currentConnection => checkConnectionConfig( currentConnection ) ? dispatch( saveCurrentConnection( Object.assign( {}, currentConnection ) ) ) : null
 });
+
+function isPositiveInteger(s)
+{
+    return /^\+?[1-9][\d]*$/.test(s);
+}
+
+/**
+ * Checks if a connection config provides valid values i.e. name, host and passwort contains content and port is
+ * a positive integer.
+ * 
+ * @param {Object} currentConnection 
+ * @returns {Boolean}
+ */
+function checkConnectionConfig( currentConnection )
+{
+    let isValid = true;
+
+    if( !currentConnection.name.length )
+    {
+        isValid = false;
+    }
+    if( !currentConnection.host.length )
+    {
+        isValid = false;
+    }
+    if( !currentConnection.password.length )
+    {
+        isValid = false;
+    }
+
+    if( !isPositiveInteger( currentConnection.port ) )
+    {
+        isValid = false;
+    }
+
+    if( currentConnection.password !== currentConnection.passwordRepeat )
+    {
+        isValid = false;
+    }
+
+    return isValid;
+}
 
 export default connect( mapStateToProps, mapDispatchToProps )( ConnectionConfig );
