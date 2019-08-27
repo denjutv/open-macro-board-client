f018bd35d543f3a9b39c9a7b6118896ce48b82a6
denjutv <info@denju.tv>
2019-08-27 21:47:09 +0200

    added translations for board settings

--- render/src/component/main/buttonSettings.js
+++ render/src/component/main/buttonSettings.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { withTranslation } from "react-i18next";
 
 function ButtonSettings( props )
 {
+    const {t} = props;
+
     return (
         <div className="main__builder">
 
@@ -9,7 +12,7 @@ function ButtonSettings( props )
 
                 {/* <!-- head --> */}
                 <div className="openDeck__name">
-                    Einstellungen
+                    {t("buttonSettingsHeadline")}
                 </div>
 
                 <div className="deckBuilder__outer">
@@ -17,30 +20,30 @@ function ButtonSettings( props )
 
                             <div className="deckForm__input">
                                 <input type="text" value="YouTube" />
-                                <label htmlFor="test1">Name</label>
+                                <label htmlFor="test1">{t("buttonSettingsName")}</label>
                             </div>
 
                         <div className="deckForm__group">
 
                             <div className="deckForm__head">
-                                <span>image</span>Icon
+                                <span>image</span>{t("buttonSettingsIcon")}
                             </div>
 
                             <div className="deckForm__range">
                                 <input type="range" value="0.6" min="0" max="2" step="0.05" />
-                                <label htmlFor="range">Skalieren</label>
+                                <label htmlFor="range">{t("buttonSettingsScale")}</label>
                                 <output>0.6</output>
                             </div>
 
                             <div className="deckForm__range">
                                 <input type="range" value="-15" min="-200" max="200" step="1" />
-                                <label htmlFor="range">Ausrichtung Y</label>
+                                <label htmlFor="range">{t("buttonSettingsTranslation")} X</label>
                                 <output>-15</output>
                             </div>
 
                             <div className="deckForm__range">
                                 <input type="range" value="0" min="-200" max="200" step="1" />
-                                <label htmlFor="range">Ausrichtung X</label>
+                                <label htmlFor="range">{t("buttonSettingsTranslation")} Y</label>
                                 <output>0</output>
                             </div>
 
@@ -49,34 +52,34 @@ function ButtonSettings( props )
                         <div className="deckForm__group">
 
                             <div className="deckForm__head">
-                                <span>payment</span>Label
+                                <span>payment</span>{t("buttonSettingsLabel")}
                             </div>
 
 
                             <div className="deckForm__range">
                                 <input type="range" value="1" min="0" max="2" step="0.05" />
-                                <label htmlFor="range">Skalieren</label>
+                                <label htmlFor="range">{t("buttonSettingsScale")}</label>
                                 <output>1</output>
                             </div>
 
                             <div className="deckForm__range">
                                 <input type="range" value="25" min="-200" max="200" step="1" />
-                                <label htmlFor="range">Ausrichtung Y</label>
+                                <label htmlFor="range">{t("buttonSettingsTranslation")} X</label>
                                 <output>25</output>
                             </div>
 
                             <div className="deckForm__range">
                                 <input type="range" value="0" min="-200" max="200" step="1" />
-                                <label htmlFor="range">Ausrichtung X</label>
+                                <label htmlFor="range">{t("buttonSettingsTranslation")} Y</label>
                                 <output>0</output>
                             </div>
 
                         </div>
 
                     </div>
-                    <div className="openDeck__new">
+                    {/* <div className="openDeck__new">
                         <span>irgendwas</span>
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
@@ -85,4 +88,4 @@ function ButtonSettings( props )
     );
 }
 
-export default ButtonSettings;
+export default withTranslation()(ButtonSettings);
