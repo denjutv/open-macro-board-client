624edbb371c8d9e001cec32d5eebb7f8c5711b74
denjutv <info@denju.tv>
2019-12-03 20:45:35 +0100

    implemented update for text and textarea inputs

--- render/src/component/inputs/text.js
+++ render/src/component/inputs/text.js
@@ -5,7 +5,7 @@ function Text(props)
     return (
         <div className="deckForm__group">
             <div className="deckForm__input">
-                <input id={props.name} name={props.name} type="text" value={props.value} />
+                <input id={props.name} name={props.name} type="text" value={props.value} onChange={props.updateInput.bind(null,props.connectionName,props.selectedButtonIndex)} />
                 <label htmlFor={props.name}>{props.label}</label>
             </div>
         </div>
--- render/src/component/inputs/textarea.js
+++ render/src/component/inputs/textarea.js
@@ -5,7 +5,7 @@ function Textarea(props)
     return (
         <div className="deckForm__group">
             <div className="deckForm__textarea">
-                <textarea id={props.name} name={props.name} value={props.value} />
+                <textarea id={props.name} name={props.name} value={props.value} onChange={props.updateInput.bind(null,props.connectionName,props.selectedButtonIndex)} />
                 <label htmlFor={props.name}>{props.label}</label>
             </div>
         </div>
